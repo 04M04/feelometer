6ebb3db4f8a2dd8691a901f4288f6b6bcbbb79bb
04M04 <micsch04@gmail.com>
2023-06-24 23:48:14 +0200

    add a new value to the backend api

--- backend/main.py
+++ backend/main.py
@@ -3,10 +3,14 @@ from pydantic import BaseModel
 import uvicorn
 
 app = FastAPI()
-feeling: str = "⚡️"
+# feeling: str = "⚡️"
+# feeling_count: int = 0
 
-class FeelingUpdate(BaseModel):
+class feeling_model(BaseModel):
     feeling: str
+    feeling_count: int
+
+feeling_state : feeling_model = {"feeling": "⚡️", "feeling_count": 0}
 
 @app.get("/")
 async def index():
@@ -14,13 +18,14 @@ async def index():
 
 @app.get("/feeling")
 async def root():
-    return {"feeling": feeling}
+    # global feeling_state
+    return feeling_state
 
 @app.put("/feeling/update")
-async def create_or_update_feeling(feeling_update: FeelingUpdate) -> str:
-    global feeling
-    feeling = feeling_update.feeling
-    return feeling
+async def create_or_update_feeling(feeling_update: feeling_model) -> feeling_model:
+    global feeling_state
+    feeling_state = feeling_update
+    return feeling_state
 
 if __name__ == "__main__":
     uvicorn.run("main:app", host="127.0.0.1", port=8000, reload=True, log_level="info")
